83b4b1bd39c4bef55b9f401709890faf2cbfa937
Mikiiill <iMikill@Lackraj.com>
2025-08-27 11:08:39 -0400

    Update RANKUP.js

--- RANKUP.js
+++ RANKUP.js
@@ -1,5 +1,4 @@
 function initiateStyleSelection() {
-    Log.debug("Initiating style selection");
     let controls = document.getElementById("style-controls");
     controls.innerHTML = "";
     let styles = ["Ninjutsu", "Genjutsu", "Taijutsu", "Fire", "Lightning", "Earth"];
@@ -13,10 +12,10 @@ function initiateStyleSelection() {
 }
 
 function selectStyle(style, button) {
-    if (game.gameState === "chooseStyles" && game.player.ninjaStyles[style] && game.player.ninjaStyles[style] === "D-Rank" && Object.values(game.player.ninjaStyles).filter(r => r !== "D-Rank").length < 2) {
-        if (confirm(`Upgrade <span class='output-text-${style.toLowerCase()}'>${style}</span> to C-Rank?`)) {
+    if (game.gameState === "chooseStyles" && game.player.ninjaStyles[style] && game.player.ninjaStyles[style] === "D-Rank" && Object.values(game.player.ninjaStyles).length < 2) {
+        if (confirm(`Upgrade ${style} to C-Rank?`)) {
             game.player.ninjaStyles[style] = "C-Rank";
-            queueOutput(`<span class='output-text-${style.toLowerCase()}'>${style}</span> trained to C-Rank!`);
+            queueOutput(`<span class="output-text-${style.toLowerCase()}">${style}</span> trained to C-Rank!`);
             button.className += " disabled";
             button.disabled = true;
             if (Object.values(game.player.ninjaStyles).filter(r => r !== "D-Rank").length === 2) {
@@ -24,21 +23,19 @@ function selectStyle(style, button) {
                 setupInitialJutsuSelection();
             } else {
                 let remainingPoints = 2 - Object.values(game.player.ninjaStyles).filter(r => r !== "D-Rank").length;
-                queueOutput(`<span class='output-text-neutral'>You have ${remainingPoints} point${remainingPoints === 1 ? '' : 's'} remaining to upgrade another style.</span>`);
+                queueOutput(`You have ${remainingPoints} point${remainingPoints === 1 ? '' : 's'} remaining to upgrade another style.`);
             }
         }
     }
 }
 
 function setupInitialJutsuSelection() {
-    Log.debug("Setting up initial Jutsu selection");
     game.gameState = "chooseInitialJutsu";
-    performJutsuSelection(3, () => ArriveVillage("Newb Village"));
+    performJutsuSelection(3, true, () => ArriveVillage("Newb Village"));
 }
 
-function performJutsuSelection(times, callback) {
+function performJutsuSelection(times, isInitial = false, callback = () => {}) {
     if (times > 0) {
-        Log.debug(`Performing Jutsu selection, remaining times: ${times}`);
         let controls = document.getElementById("jutsu-controls");
         controls.innerHTML = "";
         let skillSet = new Skills();
@@ -52,36 +49,47 @@ function performJutsuSelection(times, callback) {
             randomJutsu.forEach(jutsu => {
                 let button = document.createElement("button");
                 button.innerText = jutsu.name;
-                button.onclick = function() { selectJutsu(jutsu, () => performJutsuSelection(times - 1, callback)); };
+                button.onclick = function() { selectJutsu(jutsu, () => performJutsuSelection(times - 1, isInitial, callback)); };
                 controls.appendChild(button);
             });
         } else {
-            queueOutput("<span class='output-text-neutral'>No jutsu available based on styles and ranks.</span>");
+            queueOutput('No jutsu available based on styles and ranks.');
             if (callback) callback();
         }
     } else {
-        Log.debug("Jutsu selection completed");
+        document.getElementById("jutsu-controls").innerHTML = "";
+        if (isInitial) {
+            addInitialBarrageCards();
+        }
         if (callback) callback();
     }
 }
 
 function selectJutsu(jutsu, callback) {
     if (game.gameState === "chooseInitialJutsu" || game.gameState === "postBattle") {
-        if (confirm(`Select <span class='output-text-${jutsu.style}'>${jutsu.name}</span>?`)) {
+        if (confirm("Select " + jutsu.name + "?")) {
             let count = game.player.skills.filter(s => s.name === jutsu.name).length + game.player.skillInventory.filter(s => s.name === jutsu.name).length;
             if (count < 3 || (game.player.skills.length + game.player.skillInventory.length < 10 && (jutsu.rank === "D-Rank" || jutsu.rank === "C-Rank"))) {
                 game.player.skillInventory.push(jutsu);
-                queueOutput(`<span class='output-text-${jutsu.style}'>${jutsu.name}</span> added to skill inventory!`);
+                queueOutput(`${jutsu.name} added to skill inventory!`);
                 updateSkillCount();
                 document.getElementById("jutsu-controls").innerHTML = "";
                 if (callback) callback();
             } else {
                 game.player.gold += jutsu.rank === "D-Rank" ? 5 : jutsu.rank === "C-Rank" ? 10 : 50;
-                queueOutput(`Extra <span class='output-text-${jutsu.style}'>${jutsu.name}</span> converted to ${jutsu.rank === "D-Rank" ? 5 : jutsu.rank === "C-Rank" ? 10 : 50} gold due to owning 3 or more!`);
+                queueOutput(`Extra ${jutsu.name} converted to ${jutsu.rank === "D-Rank" ? 5 : jutsu.rank === "C-Rank" ? 10 : 50} gold due to owning 3 or more!`);
                 updateSkillCount();
                 document.getElementById("jutsu-controls").innerHTML = "";
                 if (callback) callback();
             }
         }
     }
+}
+
+function addInitialBarrageCards() {
+    let skillSet = new Skills();
+    let barrageSkill = skillSet.findSkill("Barrage");
+    game.player.skillInventory.push(barrageSkill);
+    game.player.skillInventory.push(barrageSkill);
+    queueOutput("You received 2 free Barrage cards to start!");
 }
