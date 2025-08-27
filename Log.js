be0575d20f4fbdb9f500fe60c8cce405a23e90e9
Mikiiill <iMikill@Lackraj.com>
2025-08-26 20:27:45 -0400

    Update Log.js

--- Log.js
+++ Log.js
@@ -1,12 +1,12 @@
 const Log = {
     debug: function(message) {
-        let debugMessage = `<span class='debug-log'>[DEBUG ${new Date().toLocaleTimeString('en-US', { timeZone: 'America/New_York' })}]: ${message}</span>`;
+        let debugMessage = `<span class='debug-log'>[DEBUG]: ${message}</span>`;
         game.output.push(debugMessage);
         document.getElementById("output").innerHTML = game.output.join("<br>");
         document.getElementById("output").scrollTop = document.getElementById("output").scrollHeight;
     },
     error: function(message) {
-        let errorMessage = `<span class='debug-log'>[ERROR ${new Date().toLocaleTimeString('en-US', { timeZone: 'America/New_York' })}]: ${message}</span>`;
+        let errorMessage = `<span class='error-log'>[ERROR]: ${message}</span>`;
         game.output.push(errorMessage);
         document.getElementById("output").innerHTML = game.output.join("<br>");
         document.getElementById("output").scrollTop = document.getElementById("output").scrollHeight;
