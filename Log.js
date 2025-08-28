88a35d5290cd5f5799f6e1575d831c010e28b372
Mikiiill <iMikill@Lackraj.com>
2025-08-28 15:51:22 -0400

    Update Log.js

--- Log.js
+++ Log.js
@@ -1,14 +1,4 @@
-const Log = {
-    debug: function(message) {
-        let debugMessage = `<span class='debug-log'>[DEBUG]: ${message}</span>`;
-        game.output.push(debugMessage);
-        document.getElementById("output").innerHTML = game.output.join("<br>");
-        document.getElementById("output").scrollTop = document.getElementById("output").scrollHeight;
-    },
-    error: function(message) {
-        let errorMessage = `<span class='error-log'>[ERROR]: ${message}</span>`;
-        game.output.push(errorMessage);
-        document.getElementById("output").innerHTML = game.output.join("<br>");
-        document.getElementById("output").scrollTop = document.getElementById("output").scrollHeight;
-    }
-};
+function queueOutput(text) {
+    var log = document.getElementById("battle-log");
+    if (log) log.innerHTML += "<p>" + text + "</p>";
+}
