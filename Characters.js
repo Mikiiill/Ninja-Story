d26c54bac40e15f5f3701b9bc7cea95aeac82d6c
Mikiiill <iMikill@Lackraj.com>
2025-08-30 05:21:24 -0400

    Update Characters.js

--- Characters.js
+++ Characters.js
@@ -1,21 +1,16 @@
-var enemies = {
-    "Training Dummy": { hp: 10, maxHp: 10, skills: ["Healing Stance"], skillInventory: [], statusEffects: [], lastVillage: "Newb Village" },
-    "Thief": { hp: 15, maxHp: 15, skills: ["Steal"], skillInventory: [], statusEffects: [], lastVillage: "Newb Village" },
-    "Rabid Dog": { hp: 12, maxHp: 12, skills: ["Bite"], skillInventory: [], statusEffects: [], lastVillage: "Wilderness" }
-};
-
-function Skills() {
-    this.skills = window.jutsu;
+class Skills {
+    constructor() {}
+    findSkill(name) {
+        return window.jutsu[name] ? Object.assign({}, window.jutsu[name]) : null;
+    }
 }
 
-Skills.prototype.findSkill = function(name) {
-    return this.skills[name] ? Object.assign({}, this.skills[name]) : null;
+var enemies = {
+    "Training Dummy": { name: "Training Dummy", hp: 6, maxHp: 6, skills: ["Healing Stance"], skillInventory: [], statusEffects: [], lastVillage: "Newb Village" },
+    "Thief": { name: "Thief", hp: 10, maxHp: 10, skills: ["Barrage", "Barrage", "Substitution Jutsu"], skillInventory: [], statusEffects: [], lastVillage: "Newb Village" },
+    "Rabid Dog": { name: "Rabid Dog", hp: 8, maxHp: 8, skills: ["Bite"], skillInventory: [], statusEffects: [], lastVillage: "Newb Village", ninjaStyles: { Feral: "C-Rank" } }
 };
 
 function generateEnemy(name) {
-    var enemyTemplate = enemies[name];
-    if (!enemyTemplate) return null;
-    var enemy = Object.assign({}, enemyTemplate);
-    enemy.skills = enemy.skills.map(skill => new Skills().findSkill(skill)).filter(skill => skill !== null);
-    return enemy;
+    return Object.assign({}, enemies[name]);
 }
